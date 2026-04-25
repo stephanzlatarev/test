43f4e53bedce3eb39fd93d1ae54f69bd72ebaf30
Stephan Zlatarev <stephan.zlatarev@gmail.com>
2026-04-25 12:59:58 +0300

    Add space skill

---
+++ gym/skill/space/playbook-approach.js
@@ -0,0 +1,39 @@
+
+export default function() {
+  const actor = getRandomPosition();
+  const targets = [getRandomPosition(), getRandomPosition()];
+  const target = findClosesTarget(actor, targets);
+
+  return {
+    observe: {
+      actors: [ [actor.x, actor.y] ],
+      targets: targets.map(target => [target.x, target.y]),
+    },
+    act: {
+      actors: [ [target.x, target.y] ],
+    }
+  };
+}
+
+function getRandomPosition() {
+  return {
+    x: Math.random() * 100,
+    y: Math.random() * 100,
+  };
+}
+
+function findClosesTarget(actor, targets) {
+  let closestDistance = Infinity;
+  let closestTarget = null;
+
+  for (const target of targets) {
+    const squareDistance = ((actor.x - target.x) * (actor.x - target.x) + (actor.y - target.y) * (actor.y - target.y));
+
+    if (squareDistance < closestDistance) {
+      closestDistance = squareDistance;
+      closestTarget = target;
+    }
+  }
+
+  return closestTarget;
+}
